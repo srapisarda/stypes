d5410c9ccb2775222a6aa032c4f074b5c3fc9504
Salvatore Rapisarda <salvatorer@halfordsmedia.co.uk>
2017-04-24 23:24:01 +0100

    added comment

--- src/main/scala/uk/ac/bbk/dcs/stypes/Bag.scala
+++ src/main/scala/uk/ac/bbk/dcs/stypes/Bag.scala
@@ -4,9 +4,11 @@ import fr.lirmm.graphik.graal.api.core.Atom
 import scala.collection.JavaConverters._
 
 /**
-  * Created by
+  * Created by:
+  *   Salvatore Rapisarda
+  *   Stanislav Kikot
   *
-  *     rapissal on 24/04/2017.
+  * on 27/03/2017.
   */
 case class Bag( atoms:Set[Atom]  ) {
   val variables = atoms.flatMap( p=> p.getTerms.asScala)
