325b1cffdb9af5b86501aff10dfb1037d76dd6de
Stanislav Kikot <kikot@dcs.bbk.ac.uk>
2018-01-30 17:12:06 +0000

    queries to hypergraphs script

---
+++ src/main/resources/benchmark/Tools/create_hg.py
@@ -0,0 +1,47 @@
+import sys;
+
+body = False;
+insideVariableName = False;
+vars = set()
+edges = []
+c = sys.stdin.read(1)
+while c :
+    if c == '-' and d == '<' :
+        body = True;
+    
+    if body :
+        if c == '(':
+            currentEdge = []
+    
+        if c == '?' :
+            insideVariableName = True;
+            currentVariable = "";
+       
+        if insideVariableName and (c in ",) "):
+            insideVariableName = False
+            currentEdge.append(currentVariable)
+            vars.add(currentVariable)
+            
+        if c == ')' :
+            edges.append(currentEdge)
+        
+        if insideVariableName : 
+            currentVariable = currentVariable + c;
+            
+    d = c;
+    c = sys.stdin.read(1)
+    
+    
+############# Printing Answer #######################
+for currentVariable in vars:
+    print('vertex(', currentVariable, ').');
+print()    
+for currentEdge in edges :
+    print('edge(', ",".join(currentEdge), ').');
+    
+
+
+
+
+
+
