ef9b25a5abf225fe8bce4a22af1a4ac9b0176d84
srapisarda <rapisarda.salvatore@gmail.com>
2018-04-04 23:04:26 +0100

    test for createAdditionalRules.

--- src/test/scala/uk/ac/bbk/dcs/stypes/ReWriterTest.scala
+++ src/test/scala/uk/ac/bbk/dcs/stypes/ReWriterTest.scala
@@ -418,7 +418,11 @@ class ReWriterTest extends FunSpec {
       // assert(datalog.lengthCompare(1) == 0)
     }
 
-
+    it("should create additional rules"){
+      val rules = ReWriter.createAdditionalRules(ontTGDsAll)
+      println(rules.mkString("\n"))
+      assert(23==rules.length)
+    }
 
     ignore("should rewrite query q11.cq  using 100 ont of all-tdgs.dlp") {
       val  result: (TreeDecomposition, List[Variable]) = TreeDecomposition.getTreeDecomposition(s"src/main/resources/q11.gml", "src/main/resources/q11.txt")
