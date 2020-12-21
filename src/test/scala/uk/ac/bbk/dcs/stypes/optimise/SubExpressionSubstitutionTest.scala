1984338cdc983c42e0241ccb783d0769d6156712
srapisarda <rapisarda.salvatore@gmail.com>
2020-12-21 23:29:52 +0000

    SubExpressionSubstitutionTest refactoring

--- src/test/scala/uk/ac/bbk/dcs/stypes/optimise/SubExpressionSubstitutionTest.scala
+++ src/test/scala/uk/ac/bbk/dcs/stypes/optimise/SubExpressionSubstitutionTest.scala
@@ -3,38 +3,33 @@ package uk.ac.bbk.dcs.stypes.optimise
 import org.scalatest.FunSuite
 import uk.ac.bbk.dcs.stypes.{Clause, ReWriter}
 
+import scala.io.Source
+
 class SubExpressionSubstitutionTest extends FunSuite {
 
   test("it should find the two sub expressions in the clauses body") {
-    val datalog: List[Clause] = ReWriter.getDatalogRewriting("src/test/resources/rewriting/optimise/sub-expression-substitution-test-01.dlp")
-    println(s"datalog to optimise:\n${datalog.mkString("\n")}")
-
-    val optimised = SubExpressionSubstitution.optimise(datalog, CatalogStatistics(Map()))
-    println(s"\ndatalog to optimised by sub-expression substitution:\n${optimised.mkString("\n")}")
-
-    val comp = optimised.map(_.head).find(_.getPredicate.getIdentifier.equals("comp_0"))
-    assert(comp.isDefined)
+    executeTest("src/test/resources/rewriting/optimise/sub-expression-substitution-test-01")
   }
 
   test("it should find the two sub expressions in the clauses body with different variables") {
-    val datalog: List[Clause] = ReWriter.getDatalogRewriting("src/test/resources/rewriting/optimise/sub-expression-substitution-test-02.dlp")
-    println(s"datalog to optimise:\n${datalog.mkString("\n")}")
-
-    val optimised = SubExpressionSubstitution.optimise(datalog, CatalogStatistics(Map()))
-    println(s"\ndatalog to optimised by sub-expression substistion:\n${optimised.mkString("\n")}")
-
-    val comp = optimised.map(_.head).find(_.getPredicate.getIdentifier.equals("comp_0"))
-    assert(comp.isDefined)
+    executeTest("src/test/resources/rewriting/optimise/sub-expression-substitution-test-02")
   }
 
   test("it should find the two sub expressions in the clauses body with different atoms and variables") {
-    val datalog: List[Clause] = ReWriter.getDatalogRewriting("src/test/resources/rewriting/optimise/sub-expression-substitution-test-03.dlp")
+    executeTest("src/test/resources/rewriting/optimise/sub-expression-substitution-test-03")
+  }
+
+  private def executeTest(fileName: String): Unit = {
+    val datalog: List[Clause] = ReWriter.getDatalogRewriting(s"$fileName.dlp")
     println(s"datalog to optimise:\n${datalog.mkString("\n")}")
 
     val optimised = SubExpressionSubstitution.optimise(datalog, CatalogStatistics(Map()))
-    println(s"\ndatalog to optimised by sub-expression substistion:\n${optimised.mkString("\n")}")
+    val actual = optimised.mkString("\n")
+    println(s"\ndatalog to optimised by sub-expression substistion:\n$actual")
 
-    val comp = optimised.map(_.head).find(_.getPredicate.getIdentifier.equals("comp_0"))
-    assert(comp.isDefined)
+    val source = Source.fromFile(s"$fileName-res.txt")
+    val expected = source.getLines().mkString("\n")
+    source.close()
+    assert(expected == actual)
   }
 }
