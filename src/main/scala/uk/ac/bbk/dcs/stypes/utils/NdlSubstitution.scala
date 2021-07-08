4f552b8874bb897124d482bb49375ea992748fa8
salvorapisarda <salvo.rapisarda@sainsbury.co.uk>
2021-07-08 22:06:30 +0100

    feat: improved by substituting IDB with multiple declaration
    for example substituting p1 in NDL:
    p(X):-a(X),b(X).
    p(X):-s(X).
    q(X, Y) :- a(X), p(X), r(X, Y), p(Y), b(Y).

--- src/main/scala/uk/ac/bbk/dcs/stypes/utils/NdlSubstitution.scala
+++ src/main/scala/uk/ac/bbk/dcs/stypes/utils/NdlSubstitution.scala
@@ -1,51 +1,56 @@
 package uk.ac.bbk.dcs.stypes.utils
 
-import fr.lirmm.graphik.graal.api.core.Atom
-import fr.lirmm.graphik.graal.core.DefaultAtom
+import fr.lirmm.graphik.graal.api.core.{Atom, Substitution}
+import fr.lirmm.graphik.graal.core.{DefaultAtom, TreeMapSubstitution}
 import uk.ac.bbk.dcs.stypes.Clause
 
 import scala.collection.JavaConverters._
 import scala.language.postfixOps
 
 object NdlSubstitution {
-  private def bodySubstitution(substitutionClause: Clause, clauseBody: List[Atom]): List[Atom] = {
-    val zipTermsWithIndexSubstitution = ((Stream from 0) zip substitutionClause.head.getTerms.asScala) toMap
+  private def bodySubstitutions(substitutionClauses: List[Clause], clauseBody: List[Atom]): List[List[Atom]] = {
 
-    val bodySubstituted: List[Atom] = clauseBody
-      .flatten(atom => {
-        if (atom.getPredicate == substitutionClause.head.getPredicate) {
-          val substitutionMap = atom.getTerms.asScala
-            .zipWithIndex
-            .map(termZipped => (zipTermsWithIndexSubstitution(termZipped._2), termZipped._1)).toMap
+    def bodySubstitutionH(substitutionClause: Clause) = {
+      val zipTermsWithIndexSubstitution = ((Stream from 0) zip substitutionClause.head.getTerms.asScala) toMap
 
-          substitutionClause.body.map(atomToSubstitute => {
-            val newAtom = new DefaultAtom(atomToSubstitute)
-            atomToSubstitute.getTerms.asScala.zipWithIndex.foreach(termIndexed => {
-              if (substitutionMap.contains(termIndexed._1)) {
-                newAtom.setTerm(termIndexed._2, substitutionMap(termIndexed._1))
-              }
+      val bodySubstituted: List[Atom] = clauseBody
+        .flatten(atom => {
+          if (atom.getPredicate == substitutionClause.head.getPredicate) {
+            val substitutionMap = atom.getTerms.asScala
+              .zipWithIndex
+              .map(termZipped => (zipTermsWithIndexSubstitution(termZipped._2), termZipped._1)).toMap
+
+            substitutionClause.body.map(atomToSubstitute => {
+              val newAtom = new DefaultAtom(atomToSubstitute)
+              atomToSubstitute.getTerms.asScala.zipWithIndex.foreach(termIndexed => {
+                if (substitutionMap.contains(termIndexed._1)) {
+                  newAtom.setTerm(termIndexed._2, substitutionMap(termIndexed._1))
+                }
+              })
+              newAtom
             })
-            newAtom
-          })
-        } else {
-          List(atom)
-        }
-      })
+          } else {
+            List(atom)
+          }
+        })
 
-    bodySubstituted.distinct
+      bodySubstituted.distinct
+    }
+
+    substitutionClauses.map(bodySubstitutionH)
   }
 
   def idbPredicateSubstitution(ndl: List[Clause], predicateIdentifier: String): List[Clause] = {
     val iDbPredicates = ndl.groupBy(_.head.getPredicate)
     val idbSubstitutionOption = iDbPredicates.find(_._1.getIdentifier.toString == predicateIdentifier)
-    if (idbSubstitutionOption.isEmpty || idbSubstitutionOption.get._2.size > 1) {
-      throw new RuntimeException(s"Predicate $predicateIdentifier does not exist or it is contained in more then one clauses.")
-    } else {
-      val substitution: List[Clause] = ndl.filterNot(_.head.getPredicate.getIdentifier.toString == predicateIdentifier)
-        .map(clause =>
-          Clause(clause.head,
-            bodySubstitution(idbSubstitutionOption.get._2.head, clause.body)))
-      substitution
+    if (idbSubstitutionOption.nonEmpty) {
+      val substitutionClauses = idbSubstitutionOption.get._2
+      ndl.filterNot(_.head.getPredicate.getIdentifier.toString == predicateIdentifier)
+        .flatten(clause => {
+          bodySubstitutions(substitutionClauses, clause.body)
+            .map(bodySubstituted => Clause(clause.head, bodySubstituted))
+        })
     }
+    else ndl
   }
 }
--- src/test/scala/uk/ac/bbk/dcs/stypes/utils/NdlSubstitutionTest.scala
+++ src/test/scala/uk/ac/bbk/dcs/stypes/utils/NdlSubstitutionTest.scala
@@ -3,18 +3,26 @@ package uk.ac.bbk.dcs.stypes.utils
 import org.scalatest.FunSpec
 import uk.ac.bbk.dcs.stypes.ReWriter
 
-class NdlSubstitutionTest  extends FunSpec {
+class NdlSubstitutionTest extends FunSpec {
+
   describe("IDB predicate substitution") {
 
     it("should substitute p(x) in example-rew-01") {
-      val fileTest = "example-rew-01"
-
-      val ndlExpected = ReWriter.getDatalogRewriting(s"src/test/resources/rewriting/substitution/$fileTest-res.dlp")
-      val ndl = ReWriter.getDatalogRewriting(s"src/test/resources/rewriting/substitution/$fileTest.dlp")
-      val actual = NdlSubstitution.idbPredicateSubstitution(ndl, "p")
-
-      assert(actual === ndlExpected)
+      executeTest( "example-rew-01")
     }
+
+    it("should substitute p(x) in example-rew-02") {
+      executeTest( "example-rew-02")
+    }
+
   }
 
+  private def executeTest(fileTest:String) = {
+    val folderPathTest = "src/test/resources/rewriting/substitution/"
+    val ndlExpected = ReWriter.getDatalogRewriting(s"$folderPathTest$fileTest-res.dlp")
+    val ndl = ReWriter.getDatalogRewriting(s"$folderPathTest$fileTest.dlp")
+    val actual = NdlSubstitution.idbPredicateSubstitution(ndl, "p")
+
+    assert(actual === ndlExpected)
+  }
 }
