a16b584b12801d6b3fc0d34d8ee591f4f863577d
salvorapisarda <salvo.rapisarda@sainsbury.co.uk>
2021-12-31 14:10:51 +0000

    added script flatten_ndl_2_sql.sh

--- scripts/ndl2sql/ndl2sql.sh
+++ scripts/ndl2sql/ndl2sql.sh
@@ -1,49 +1,9 @@
 #!/bin/bash
 
 NDL=$1
-STYPES_JAR=/home/hduser/development/stypes/target/scala-2.12/stypes-assembly-1.1.1.jar 
-STYPES_FLINK_JAR=/home/hduser/development/stypes-flink/target/scala-2.12/stypes-flink_2.12-1.0.jar
-IFS="," read -a PREDICATES <<< $3; 
-OPTIMISE=$4
-echo "flatten for:  ${PREDICATES[@]}" 
+EDB_CATALOG=$2
+STYPES_JAR=/home/hduser/development/stypes/target/scala-2.12/stypes-assembly-1.1.1.jar
+echo "ndl to SQL for:  $NDL"
 
-for PAR in 10
-do
-
-for TTL in 5
-do
-
-#/opt/flink/bin/flink run \
-#  -c uk.ac.bbk.dcs.stypes.flink.FlinkRewritingSql \
-#  -p $PAR \
-#   $STYPES_FLINK_JAR $TTL "${NDL}_par-${PAR}_ttl-${TTL}" true "$SQL"
-
-for PREDICATE in ${PREDICATES[@]}
-do
-
-NDL_FLATTEN_FILE="${NDL}_rew_${PREDICATE}.dlp"
-
-
-# shellcheck disable=SC2091
-NDL_FLATTEN=$(java -cp $STYPES_JAR  uk.ac.bbk.dcs.stypes.utils.NdlFlatten "$NDL" $PREDICATE)
-
-echo "$NDL_FLATTEN" > "$NDL_FLATTEN_FILE"
-
-SQL=$(java -cp $STYPES_JAR uk.ac.bbk.dcs.stypes.sql.SqlUtils "$NDL_FLATTEN_FILE" "$2")
+SQL=$(java -cp $STYPES_JAR uk.ac.bbk.dcs.stypes.sql.SqlUtils "$NDL" "$EDB_CATALOG")
 echo ${SQL}
-
-#java -cp $STYPES_JAR  uk.ac.bbk.dcs.stypes.utils.UtilRunner  "$SQL" abc
-
-/opt/flink/bin/flink run \
-  -c uk.ac.bbk.dcs.stypes.flink.FlinkRewritingSql \
-  -p $PAR \
-   $STYPES_FLINK_JAR $TTL "${NDL}_par-${PAR}_ttl-${TTL}_p-${PREDICATE}" $OPTIMISE  "$SQL"
-
-#echo "submitte q45  ttl: $ttl, par: $par"
-#sleep 30
-
-done
-
-done
-
-done
