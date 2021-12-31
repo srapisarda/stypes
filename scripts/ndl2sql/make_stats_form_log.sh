f2f4e18e8158d00c4234b9234e9c8c0563cddb1a
salvorapisarda <salvo.rapisarda@sainsbury.co.uk>
2021-12-31 15:55:02 +0000

    fix: make_stats_form_log.sh by adding the workdir path to jobs dir.

--- scripts/ndl2sql/make_stats_form_log.sh
+++ scripts/ndl2sql/make_stats_form_log.sh
@@ -7,13 +7,13 @@ LOG_FILE="${WORKING_DIR}/$2"
 JOBS=$(more "$LOG_FILE.log"  | grep "submitted with JobID" | cut -d ' ' -f7)
 #echo $JOBS
 
-JOBS_DIR="jobs"
+JOBS_DIR="${WORKING_DIR}/jobs"
 mkdir -p $JOBS_DIR
-rm "${JOBS_DIR}/*"
+rm -rf "${JOBS_DIR}/*"
 
 for j in $JOBS
 do
-  hadoop fs -get /flink/completed-jobs/$j "${WORKING_DIR}/${JOBS_DIR}/."
+  hadoop fs -get /flink/completed-jobs/$j "${JOBS_DIR}/."
 done
 
 JOBS_ARGS=$(join_by " ${JOBS_DIR}/" $JOBS )
