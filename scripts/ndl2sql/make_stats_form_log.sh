dba8ff9fd79ea4d6176e3fe2afc3c752683a2fb9
salvorapisarda <salvo.rapisarda@sainsbury.co.uk>
2021-12-31 15:20:14 +0000

    fix: make_stats_form_log.sh

--- scripts/ndl2sql/make_stats_form_log.sh
+++ scripts/ndl2sql/make_stats_form_log.sh
@@ -3,8 +3,8 @@
 function join_by { local d=${1-} f=${2-}; if shift 2; then printf %s "$f" "${@/#/$d}"; fi; }
 
 WORKING_DIR=$1
-LOG_FILE="${WORKING_DIR}/$2.log"
-JOBS=$(more $LOG_FILE  | grep "submitted with JobID" | cut -d ' ' -f7)
+LOG_FILE="${WORKING_DIR}/$2"
+JOBS=$(more "$LOG_FILE.log"  | grep "submitted with JobID" | cut -d ' ' -f7)
 #echo $JOBS
 
 JOBS_ARGS=$(join_by ' ' $JOBS )
@@ -13,8 +13,8 @@ do
   hadoop fs -get /flink/completed-jobs/$j "${WORKING_DIR}/."
 done
 
-CSV_STATS_FILE="${WORKING_DIR}/${LOG_FILE}.csv"
-python3 get_job_statistic.py -o "$CSV_STATS_FILE" -j $JOBS_ARGS
+CSV_STATS_FILE="${WORKING_DIR}/${LOG_FILE}"
+python3 get_job_statistic.py -o "$CSV_STATS_FILE.csv" -j $JOBS_ARGS
 
 
 
