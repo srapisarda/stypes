22a82557fb1e96ad1ee482df92fb4ef605b2e3a0
salvorapisarda <salvo.rapisarda@sainsbury.co.uk>
2022-01-05 21:28:38 +0000

    feat: made edb catalog file independent of the working dir in ndl2sql.sh

--- scripts/ndl2sql/ndl2sql.sh
+++ scripts/ndl2sql/ndl2sql.sh
@@ -2,7 +2,8 @@
 
 WORKING_DIR=$1
 NDL_FILE="${WORKING_DIR}/${2}.dlp"
-EDB_CATALOG="${WORKING_DIR}/${3}.dlp"
+SQL_FILE="${WORKING_DIR}/${2}.sql"
+EDB_CATALOG="$3"
 STYPES_JAR=/home/hduser/development/stypes/target/scala-2.12/stypes-assembly-1.1.1.jar
 
 echo "$NDL_FILE to SQL"
@@ -10,7 +11,6 @@ echo "$NDL_FILE to SQL"
 SQL=$(java -cp $STYPES_JAR uk.ac.bbk.dcs.stypes.sql.SqlUtils "$NDL_FILE" "$EDB_CATALOG")
 echo ${SQL}
 
-SQL_FILE="${NDL_FILE}.sql"
 echo "$SQL" > "$SQL_FILE"
 
 echo SQL file "$NDL_FILE" has been written
