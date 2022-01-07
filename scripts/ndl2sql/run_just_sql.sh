61dceb323caeb6e1b6ebb9cb991872b13e960202
srapisarda <rapisarda.salvatore@gmail.com>
2022-01-07 00:04:36 +0000

    test: executed evaluation test on q15

--- scripts/ndl2sql/run_just_sql.sh
+++ scripts/ndl2sql/run_just_sql.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+isrunning() {
+   if /opt/flink/bin/flink list | grep -q  "No running jobs."    
+   then 
+       return 1 
+    else 
+       return 0 
+    fi
+}
+
+
 WORKING_DIR=$1
 SQL_FILE="${WORKING_DIR}/${2}.sql"
 STYPES_FLINK_JAR=/home/hduser/development/stypes-flink/target/scala-2.12/stypes-flink_2.12-1.0.jar
@@ -14,6 +24,11 @@ do
 SQL=$(cat "$SQL_FILE")
 echo ${SQL}
 
+while isrunning 
+do
+  echo "waiting..."
+  sleep 10
+done
 
 /opt/flink/bin/flink run \
   -c uk.ac.bbk.dcs.stypes.flink.FlinkRewritingSql \
