49bb4177add3c268cfa9ded05b86c4c379f1a041
salvo.rapisarda <salvo.rapisarda@sainsburys.co.uk>
2022-03-03 22:13:55 +0000

    feat: added variation (in percentage) for duration and task from the minimum registered

--- scripts/ndl2sql/csv_eval_analysis.py
+++ scripts/ndl2sql/csv_eval_analysis.py
@@ -3,7 +3,7 @@ import re
 from pathlib import Path
 from shutil import rmtree
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import input_file_name, regexp_replace, regexp_extract, avg, round
+from pyspark.sql.functions import input_file_name, regexp_replace, regexp_extract, avg, round, max, min
 
 
 def __main(spark: SparkSession):
@@ -23,13 +23,38 @@ def __main(spark: SparkSession):
         .withColumn("evaluation",
                     regexp_replace(regexp_extract(input_file_name(), '([^\/]+$)', 0), '.csv|rew_|flatten_|-eval', ''))
 
+    # .join(df_total, ['job-parallelism', 'data-set', 'evaluation'])\
+
     df = df.select(df['duration'], df['total'].alias('tasks'), df['job-parallelism'], df['data-set'], df['evaluation']) \
         .filter(df['state'] == 'FINISHED') \
         .groupBy('job-parallelism', 'data-set', 'evaluation') \
         .agg(round(avg('duration'), 0).alias('duration'), round(avg('tasks'), 0).alias('tasks')) \
         .orderBy('data-set', 'job-parallelism', 'evaluation')
 
-    df.coalesce(1).write.csv(csv_file_path, header='true')
+    df.show()
+
+    df_min_max = df.select(df['duration'], df['tasks'], df['job-parallelism'], df['data-set']) \
+        .groupBy('job-parallelism', 'data-set') \
+        .agg(max('duration').alias('max-duration'), min('duration').alias('min-duration'),
+             max('tasks').alias('max-tasks'), min('tasks').alias('min-tasks')) \
+        .orderBy('data-set', 'job-parallelism')
+    df_min_max.show()
+
+    df_with_stats = df.join(df_min_max, ['job-parallelism', 'data-set']) \
+        .withColumn('duration-min-increase',
+                    round((df['duration'] - df_min_max['min-duration']) / df_min_max['min-duration'], 2)) \
+        .withColumn('duration-max-decrease',
+                    round((df_min_max['max-duration'] - df['duration']) / df_min_max['max-duration'], 2)) \
+        .withColumn('tasks-min-increase',
+                    round((df['tasks'] - df_min_max['min-tasks']) / df_min_max['min-tasks'], 2)) \
+        .withColumn('tasks-max-decrease',
+                    round((df_min_max['max-tasks'] - df['tasks']) / df_min_max['max-tasks'], 2)) \
+        .select('job-parallelism', 'data-set', 'evaluation', 'duration', 'tasks', 'duration-min-increase', 'tasks-min-increase') \:q!
+        .orderBy('data-set', 'job-parallelism')
+
+    df_with_stats.show()
+
+    df_with_stats.coalesce(1).write.csv(csv_file_path, header='true')
 
 
 if __name__ == '__main__':
@@ -42,4 +67,3 @@ if __name__ == '__main__':
             .getOrCreate()
     )
     __main(spark_session)
-
